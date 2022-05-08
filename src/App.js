f2cf5f7bef2bcd19776fbb98157eee4b6f4a5648
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-08 17:06:08 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+const App = () => {
+  const [name, setname] = useState('')
+  const [result, setresult] = useState(true)
+
+  const nameCheck = (name) => {
+    const nameRegex = /^[a-zA-Z ]{2,40}$/
+    if (nameRegex.test(name)) {
+      setresult(true)
+    } else if (name.includes('--')) {
+      setresult(false)
+    } else {
+      setresult(false)
+    }
+  }
+
+  useEffect(() => {
+    nameCheck(name)
+  }, [name])
+
+  return (
+    <div style={{ margin: '50px' }}>
+      <h1> Agile HomeWork 4</h1>
+      <h3>Ayoub Maimmadi</h3>
+      <h3>Rabii Alaui</h3>
+      <input
+        style={{
+          margin: '10px',
+          height: '50px',
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          padding: '0 10px',
+        }}
+        type="text"
+        value={name}
+        onChange={(e) => setname(e.target.value)}
+        placeholder="Enter your name"
+      />
+      <h1
+        style={{
+          color: 'blue',
+          margin: '10px',
+          padding: '0 10px',
+        }}
+        date-tested="button"
+      >
+        {name}
+      </h1>
+      <h1
+        style={{
+          color: 'red',
+          margin: '10px',
+          padding: '0 10px',
+        }}
+        date-tested="button"
+      >
+        {result ? 'Valid' : 'Invalid'}
+      </h1>
+    </div>
+  )
+}
+
+export default App
